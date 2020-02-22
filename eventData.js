9ad9094bfd95c07d43c1f1a03ad94dfa2d923bb4
manasisb <44342222+manasisb@users.noreply.github.com>
2020-02-21 21:48:16 -0800

    Added files

---
+++ eventData.js
@@ -0,0 +1,31 @@
+//Club activities
+events = [
+    { 
+        "name": "Tadoba Trip", 
+        "dates" : [" June 12", "  July 10", " August 8", " October 15", " Nov 20", " Dec 5 "]
+    },
+    {
+        "name": "Afternoon safari at Tadoba Andhari Tiger Reserve",
+        "dates": ["June 12", " July 10", " August 8", " October 15", " Nov 20", " Dec 5 "]
+    },
+    {
+        "name": "Overnight at Royal Tiger Resort, Tadoba", 
+        "dates": ["June 12", " July 10", " August 8", " October 15", " Nov 20", " Dec 5 "]
+    },
+    {
+        "name": "Start from Tadoba to Somnath after breakfast", 
+        "dates": ["June 13", " July 11", " August 9", " October 16", " Nov 21", " Dec 6 "]
+    },
+    {
+        "name": "Visit Somnath Project and Hemalkasa", 
+        "dates":["June 13", " July 11", " August 9", " October 16", " Nov 21", " Dec 6 "]
+    },
+    {
+        "name":"Morning Jungle Safari at Tadoba Andhari Tiger Reserve", 
+        "dates":["June 14", " July 12", " August 10", " October 17", " Nov 22", " Dec 7 "]
+    },
+    {
+        "name":"Visit to Anandvan",
+        "dates":["June 14", " July 12", " August 10", " October 17", " Nov 22", " Dec 7 "]
+    }
+];
